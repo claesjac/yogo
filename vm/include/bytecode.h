3ce532dc47f967d2a4a92e1f8632732599335d2f
Claes Jakobsson <claes@surfar.nu>
2012-02-28 20:49:53 +0100

    Simple assembler

--- vm/include/bytecode.h
+++ vm/include/bytecode.h
@@ -4,11 +4,15 @@
     released under BSD license
 */
 
-#define OPCODE(code, mnemonic, operands)
+#define OPCODE(code, mnemonic, operands, kind)
 
-OPCODE(0, null, 0)
-OPCODE(1, true, 0)
-OPCODE(2, false, 0)
-OPCODE(3, iload, 1)
-OPCODE(4, istore, 1)
-OPCODE(5, return, 0)
+OPCODE(  0, null,   0, )
+OPCODE(  1, true,   0, )
+OPCODE(  2, false,  0, )
+OPCODE(  3, iload,  1, )
+OPCODE(  4, istore, 1, )
+OPCODE(  5, return, 0, )
+OPCODE(  6, istrue, 1, addr)
+OPCODE(  7, iseq,   1, addr)
+OPCODE(  8, shift,  0, )
+OPCODE(  9, print,  0, )
